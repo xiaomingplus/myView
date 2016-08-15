28d282dde8cfbebe3134d54a067f41948d328687
小明plus <xiaomingplus@qq.com>
2016-08-15 09:01:56 +0800

    for update

--- example/page2.js
+++
@@ -1,7 +0,0 @@
-define('page2', function(require, exports, module) {
-    exports.first = function(view) {
-        view.setContent('<p>页面二的first方法</p><a href="#page1=first">跳转到页面一的first方法</a><br><br><a href="#page1=second">跳转到页面一的second方法</a>');
-    };
-
-
-});
